275d7e81562d6a7041ecb6ba6a9473a007008c64
Kariithi <iankariithi@gmail.com>
2026-06-25 11:03:37 +0300

    Moved config to application.properties file

--- src/main/java/org/fourthperson/Config.java
+++ src/main/java/org/fourthperson/Config.java
@@ -1,11 +1,31 @@
 package org.fourthperson;
 
-public interface Config {
-    String dbName = "pers_test";
-    String dbUser = "root";
-    String dbPass = "I@N2131";
-    String databaseUrl = "jdbc:mariadb://localhost:3306/" + Config.dbName;
-    int serverPort = 3030;
-    String evaluationError = "Invalid answer data passed. Please refer to documentation for a sample.";
-    String internalServerError = "An error occurred. Consult admin";
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+    private static final Logger logger = LoggerFactory.getLogger(Config.class);
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = Config.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                logger.warn("Sorry, unable to find application.properties");
+            }
+            properties.load(input);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
+
+    public static final String databaseUrl = properties.getProperty("db.url", "");
+    public static final String dbUser = properties.getProperty("db.user", "");
+    public static final String dbPass = properties.getProperty("db.pass", "");
+    public static final int serverPort = Integer.parseInt(properties.getProperty("server.port", "-1"));
+    public static final String internalServerError = properties.getProperty("error.internal", "Internal server error");
+    public static final String evaluationError = properties.getProperty("error.evaluation", "Evaluation error");
 }
+    
